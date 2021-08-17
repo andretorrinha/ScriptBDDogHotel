761ee9f7628bb3dd5b9c1b0e4f0ca5ee65f2c68e
andre.torrinha <andre.torrinha@riopele.pt>
2021-08-17 14:14:05 +0100

    Updated to change exp_flag value, added for iteration instead of cod repetition

--- CleanBD.py
+++ CleanBD.py
@@ -2,29 +2,34 @@ from pymongo import MongoClient
 from datetime import datetime
 from datetime import timedelta
 
+#conexão base de dados
 client = MongoClient("mongodb+srv://csmroot:H4fUhbar49gP5PAnvH9V@clusterteste.9310k.mongodb.net/casalsaomartinho?retryWrites=true&w=majority") #conexão ao cluster
-
 db = client.casalsaomartinho  #base dados casalsaomartinho
 col = db.reservations #colection reservations
+
+#####
 
 #definir os diferentes tempo para criação da flag
 del_time_cc = datetime.now() - timedelta(hours=3, minutes=0)
 del_time_mb = datetime.now() - timedelta(hours=12, minutes=0)
-del_time_pp = datetime.now() - timedelta(hours=1, minutes=0)
 del_time_mbw = datetime.now() - timedelta(hours=1, minutes=0)
+del_time_pp = datetime.now() - timedelta(hours=1, minutes=0)
 
-#testes
-print("TOTAL DOCS:", col.count_documents({}))
-print("CURRENT TIME:", del_time_cc)
+#####
 
-#queries na colection
-del_result_cc = col.delete_many({"createdAt": {"$gt": del_time_cc}, "payment":"cc"})
-del_result_mb = col.delete_many({"createdAt": {"$gt": del_time_mb}, "payment":"mb"})
-del_result_pp = col.delete_many({"createdAt": {"$gt": del_time_pp}, "payment":"pp"})
-del_result_mbw = col.delete_many({"createdAt": {"$gt":del_time_mbw}, "payment":"mbw"})
+#criação de listas com a informação dos metodos de pagamento, e dos tempos de remoção
+payment_del_time = [del_time_cc, del_time_mb, del_time_mbw, del_time_pp]
+payment_methods = ["cc", "mb", "mbw", "pp"]
 
-#resultados
-print ("delete count:", del_result_cc.deleted_count)
-print ("delete count:", del_result_mb.deleted_count)
-print ("delete count:", del_result_pp.deleted_count)
-print ("delete count:", del_result_mbw.deleted_count)
+#####
+
+#loop para realizar o update na variavel exp_flag de todos os metodos de pagamento
+for value in range(4):
+    update_result_cc = col.update_many(
+        {"createdAt": {"$gt": payment_del_time[value]}, "payment":payment_methods[value], "status":0},
+        { "$set": {"exp_flag":1}}
+    )
+    print("Found count:", update_result_cc.matched_count)
+    print("Updated count:", update_result_cc.modified_count)
+
+
