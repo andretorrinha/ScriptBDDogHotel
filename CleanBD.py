4ad29557c7996e1f13a72ea212055ade0fb34f89
andre.torrinha <andre.torrinha@riopele.pt>
2021-08-18 09:40:41 +0100

    changed static connection string to protected method

--- CleanBD.py
+++ CleanBD.py
@@ -1,28 +1,32 @@
 from pymongo import MongoClient
 from datetime import datetime
 from datetime import timedelta
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_CONECTION_STRING = os.getenv('MONGO_CONECTION_STRING')
 
 #conexão base de dados
-client = MongoClient("mongodb+srv://csmroot:H4fUhbar49gP5PAnvH9V@clusterteste.9310k.mongodb.net/casalsaomartinho?retryWrites=true&w=majority") #conexão ao cluster
+client = MongoClient(MONGO_CONECTION_STRING) #conexão ao cluster
 db = client.casalsaomartinho  #base dados casalsaomartinho
 col = db.reservations #colection reservations
 
 #####
-
-#definir os diferentes tempo para ativaçao da flag
+#definir os diferentes tempos para ativaçao da flag, 
+#CUIDADO COM POTENCIAIS DIFERENÇAS EM HORAS COM O DEEPLOY NO SERVER
 del_time_cc = datetime.now() - timedelta(hours=3, minutes=0)
 del_time_mb = datetime.now() - timedelta(hours=12, minutes=0)
 del_time_mbw = datetime.now() - timedelta(hours=1, minutes=0)
 del_time_pp = datetime.now() - timedelta(hours=1, minutes=0)
 
 #####
-
 #criação de listas com a informação dos metodos de pagamento, e dos tempos de remoção
 payment_del_time = [del_time_cc, del_time_mb, del_time_mbw, del_time_pp]
 payment_methods = ["cc", "mb", "mbw", "pp"]
 
 #####
-
 #loop para realizar o update na variavel exp_flag de todos os metodos de pagamento
 for value in range(len(payment_methods)):
     update_result_cc = col.update_many(
@@ -32,5 +36,6 @@ for value in range(len(payment_methods)):
     print("Found ", payment_methods[value], " count:", update_result_cc.matched_count)
     print("Updated ", payment_methods[value], " count:", update_result_cc.modified_count)
 
+#####
 #Encerrar a conexão com a bd
 client.close()
