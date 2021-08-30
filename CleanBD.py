fd5cd9ade1a5eef876f65f4c045cf853b60fed84
andre.torrinha <andre.torrinha@riopele.pt>
2021-08-30 10:05:42 +0100

    added logging

--- CleanBD.py
+++ CleanBD.py
@@ -1,9 +1,14 @@
+import logging
 import pytz
 import os
 from datetime import datetime
 from datetime import timedelta
 from dotenv import load_dotenv
 from pymongo import MongoClient
+
+###
+#configurar logging
+logging.basicConfig(filename='app.log', level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 #####
 #Ler variavel conection_string do .env
@@ -18,7 +23,6 @@ col = db.reservations #colection reservations
 
 #####
 #definir os diferentes tempos para ativaçao da flag, 
-
 del_time_cc = datetime.now(pytz.utc) - timedelta(hours=2, minutes=0) #UTC JA É MENOS UMA HORA LOGO 2 = 3
 del_time_mb = datetime.now(pytz.utc) - timedelta(hours=11, minutes=0) # 12H
 del_time_mbw = datetime.now(pytz.utc) - timedelta(hours=1, minutes=0) # 2H
@@ -33,14 +37,19 @@ payment_methods = ["cc", "mb", "mbw", "pp"]
 
 #####
 #loop para realizar o update na variavel exp_flag de todos os metodos de pagamento
-for value in range(len(payment_methods)):
-    update_result_cc = col.update_many(
-        {"createdAt": {"$lt": payment_del_time[value]}, "payment":payment_methods[value], "status":0},
-        { "$set": {"status":9}}
-    )
-    print("Found ", payment_methods[value], " count:", update_result_cc.matched_count)
-    print("Updated ", payment_methods[value], " count:", update_result_cc.modified_count)
+try:
+    for value in range(len(payment_methods)):
+        update_result_cc = col.update_many(
+            {"createdAt": {"$lt": payment_del_time[value]}, "payment":payment_methods[value], "status":0},
+            { "$set": {"status":9}}
+        )
+        print("Found ", payment_methods[value], " count:", update_result_cc.matched_count)
+        print("Updated ", payment_methods[value], " count:", update_result_cc.modified_count)
+    logging.info("Os valores sobre o estado de pagamento levaram update")
 
+except Exception as e:
+    logging.exception("Erro a dar update na bd", exc_info=True)
+    
 #####
 #Encerrar a conexão com a bd
 client.close()
